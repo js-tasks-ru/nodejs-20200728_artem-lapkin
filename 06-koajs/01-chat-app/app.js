d3bfa45177ba730356c73a1887780fd4ecadf3d6
Artem Lapkin <artemka666@mail.ru>
2020-09-23 18:54:24 +0300

    added a solution for 06-koajs 01-chat-app task

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,30 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const clients = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise(resolve => {
+    clients.push(resolve);
+    
+    ctx.res.on('close', () => {
+      clients.splice(clients.indexOf(resolve), 1);
+      resolve('Unsubscribed');
+    });
+  });
+
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (!ctx.request.body.message) {
+    ctx.body = 'empty message';
+    return;
+  }
+  
+  clients.forEach(client => client(ctx.request.body.message));
+  clients.length = 0;
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());

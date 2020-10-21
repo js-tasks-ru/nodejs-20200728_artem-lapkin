ecc2c40f2da729067ae000d0549377d4f1a51c17
Artem Lapkin <artemka666@mail.ru>
2020-10-21 14:16:41 +0300

    added solution for 07-mongodb-mongoose 02-rest-api task

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,49 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
+const mapProduct = (product) => {
+  return {
+    id: product.id,
+    title: product.title,
+    images: product.images,
+    category: product.category,
+    subcategory: product.subcategory,
+    price: product.price,
+    description: product.description
+  }
+};
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategoryId = ctx.request.query.subcategory;
+
+  if (!subcategoryId) return next();
+  
+  if (!mongoose.Types.ObjectId.isValid(subcategoryId)) {
+    ctx.status = 400;
+    ctx.body = {error: 'invalid id'};
+    return;
+  }
+
+  const products = await Product.find({subcategory: subcategoryId});
+
+  ctx.body = {products: products.map(mapProduct)};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+
+  ctx.body = {products: products.map(mapProduct)};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const product = await Product.findById(ctx.params.id);
+
+  if(!product) {
+    ctx.status = 404;
+    ctx.body = {};
+    return;
+  }
+
+  ctx.body = {product: mapProduct(product)};
 };
 

c7d690a3f8fa514d3104beacc9cab43764c55ced
Artem Lapkin <artemka666@mail.ru>
2020-10-26 18:28:00 +0300

    added solution for 08-authentication 01-local task

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,25 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
-    },
+  {usernameField: 'email', session: false},
+  async function(email, password, done) {
+    const user = await User.findOne({email});
+    
+    if (!user) {
+      done(null, false, 'Нет такого пользователя');
+      return;
+    } else {
+      const isValid = await user.checkPassword(password);
+      
+      if (!isValid) {
+        done(null, false, 'Неверный пароль');
+        return;
+      } else {
+        done(null, user, 'Ok');
+        return;
+      }
+    }
+    done(null, false, 'Стратегия подключена, но еще не настроена');
+  },
 );

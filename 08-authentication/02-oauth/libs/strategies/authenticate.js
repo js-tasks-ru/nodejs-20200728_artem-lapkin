35224ce413434f78ae76da90dc592b39f8021255
Artem Lapkin <artemka666@mail.ru>
2020-10-28 22:39:39 +0300

    added solution for 08-authentication 02-oauth task

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,22 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) return done(null, false, `Не указан email`);
+
+  const user = await User.findOne({email});  
+  if (user) {
+    return done(null, user);
+  } else {
+    let createdUser;
+
+    try {
+      createdUser = await User.create({email: email, displayName: displayName});
+    } catch (err) {
+      return done(err);
+    }
+
+    return done(null, createdUser);
+  }
+
+  return done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
 };

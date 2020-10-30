fa9ef765f130e63008bddf5e76085176914bb218
Artem Lapkin <artemka666@mail.ru>
2020-10-30 11:39:09 +0300

    changed 'try catch' block

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -3,19 +3,16 @@ const User = require('../../models/User');
 module.exports = async function authenticate(strategy, email, displayName, done) {
   if (!email) return done(null, false, `Не указан email`);
 
-  const user = await User.findOne({email});  
-  if (user) {
-    return done(null, user);
-  } else {
-    let createdUser;
-
-    try {
-      createdUser = await User.create({email: email, displayName: displayName});
-    } catch (err) {
-      return done(err);
+  try {
+    const user = await User.findOne({email});  
+    if (user) {
+      return done(null, user);
+    } else {
+      let createdUser = await User.create({email: email, displayName: displayName});
+      return done(null, createdUser);
     }
-
-    return done(null, createdUser);
+  } catch (err) {
+    return done(err);
   }
 
   return done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
